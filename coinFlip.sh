eaa9982e2ae00dd5927035483206a9b9a20e0369
ashu3902 <moreashutosh1997@gmail.com>
2020-10-24 14:07:36 +0530

    Added: mutiple flips and final winner

--- coinFlip.sh
+++ coinFlip.sh
@@ -1,11 +1,17 @@
 #!/bin/bash -x
 echo "Welcome to Coin flip simulator problem"
-randomValue=$(($RANDOM%2+1));
-if [ $randomValue -eq 1 ]
-then
-	echo "Head"   
-        ((head++))
-else
-	echo "Tail"
-        ((tail++))
-fi
+head=0
+tail=0
+while [[ $head -lt 11 && $tail -lt 11 ]]
+do
+	randomValue=$(($RANDOM%2+1));
+	if [ $randomValue -eq 1 ]
+	then
+        	((head++))
+	else
+        	((tail++))
+	fi
+done
+echo "Head won $head times"
+echo "Tail won $tail times"
+
