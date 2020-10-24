569c5094c5508e9cb3236aef46dc6d14968de61e
ashu3902 <moreashutosh1997@gmail.com>
2020-10-24 14:11:38 +0530

    Added: changes UC2 as specified

--- coinFlip.sh
+++ coinFlip.sh
@@ -2,7 +2,7 @@
 echo "Welcome to Coin flip simulator problem"
 head=0
 tail=0
-while [[ $head -lt 11 && $tail -lt 11 ]]
+while [[ $head -lt 21 && $tail -lt 21 ]]
 do
 	randomValue=$(($RANDOM%2+1));
 	if [ $randomValue -eq 1 ]
@@ -12,6 +12,17 @@ do
         	((tail++))
 	fi
 done
-echo "Head won $head times"
-echo "Tail won $tail times"
+if [ $head -eq $tail ]
+then
+	echo "Tie"
+elif [ $head -gt $tail ]
+then
+	echo "Head won"
+	a=$(($head-$tail))
+	echo "Head won by $a flips"
+else
+	echo "Tail Won"
+	b=$(($tail-$head))
+	echo "Tail won by $b flips"
+fi
 
